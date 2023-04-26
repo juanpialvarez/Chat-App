eb2c14a64be54680b1b00f69f10ae660c9166c94
juanpialvarez <juanpabloalvarezv@gmail.com>
2023-04-25 18:15:27 -0600

    Add back-end message functionality with firebase

--- App.js
+++ App.js
@@ -1,4 +1,6 @@
 import { StyleSheet } from "react-native";
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Start from "./components/Start";
@@ -6,12 +8,27 @@ import Chat from "./components/Chat";
 
 const Stack = createNativeStackNavigator();
 
+const firebaseConfig = {
+  apiKey: "AIzaSyA_2X4BzU2clpSkcHanymMIhXWl5cJq-qc",
+  authDomain: "chatapp-80a34.firebaseapp.com",
+  projectId: "chatapp-80a34",
+  storageBucket: "chatapp-80a34.appspot.com",
+  messagingSenderId: "504923791811",
+  appId: "1:504923791811:web:9711b2ed5e8993f16d9d76",
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
 const App = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName='Start'>
         <Stack.Screen name='Start' component={Start} />
-        <Stack.Screen name='Chat' component={Chat} />
+        <Stack.Screen name='Chat'>
+          {(props) => <Chat db={db} {...props} />}
+        </Stack.Screen>
       </Stack.Navigator>
     </NavigationContainer>
   );
--- components/Chat.js
+++ components/Chat.js
@@ -1,36 +1,51 @@
 import { useEffect, useState } from "react";
 import { KeyboardAvoidingView, StyleSheet, Text, View } from "react-native";
 import { Bubble, GiftedChat } from "react-native-gifted-chat";
+import {
+  collection,
+  addDoc,
+  onSnapshot,
+  query,
+  orderBy,
+} from "firebase/firestore";
 
-const Chat = ({ route, navigation }) => {
-  const { name, color } = route.params;
+const Chat = ({ db, route, navigation }) => {
+  const { name, color, userID } = route.params;
   const [messages, setMessages] = useState([]);
+
   useEffect(() => {
     navigation.setOptions({ title: name });
-    setMessages([
-      {
-        _id: 1,
-        text: "Hello developer",
-        createdAt: new Date(),
-        user: {
-          _id: 2,
-          name: "React Native",
-          avatar: "https://placeimg.com/140/140/any",
-        },
-      },
-      {
-        _id: 2,
-        text: "This is a system message",
-        createdAt: new Date(),
-        system: true,
-      },
-    ]);
+    const q = query(collection(db, "messages"), orderBy("createdAt", "desc"));
+    const unsubMessages = onSnapshot(q, (docs) => {
+      let newMessages = [];
+      docs.forEach((doc) => {
+        newMessages.push({
+          id: doc.id,
+          ...doc.data(),
+          createdAt: new Date(doc.data().createdAt.toMillis()),
+        });
+      });
+      setMessages(newMessages);
+    });
+    return () => {
+      if (unsubMessages) unsubMessages();
+    };
   }, []);
 
-  const onSend = (newMessages) => {
-    setMessages((previousMessages) =>
-      GiftedChat.append(previousMessages, newMessages)
+  const addMessagesItem = async (newMessage) => {
+    const newMessageRef = await addDoc(
+      collection(db, "messages"),
+      newMessage[0]
     );
+    if (!newMessageRef.id) {
+      Alert.alert(
+        "There was an error sending your message. Please try again later"
+      );
+    }
+  };
+
+  const onSend = (newMessages) => {
+    addMessagesItem(newMessages);
   };
 
   const renderBubble = (props) => {
@@ -39,7 +54,8 @@ const Chat = ({ route, navigation }) => {
         {...props}
         textStyle={{
           right: {
-            color: color === "white" ? "black" : "white",
+            color:
+              (color === "white") | (color === "yellow") ? "black" : "white",
           },
         }}
         wrapperStyle={{
@@ -62,8 +78,9 @@ const Chat = ({ route, navigation }) => {
         renderBubble={renderBubble}
         onSend={(messages) => onSend(messages)}
         user={{
-          _id: 1,
+          _id: userID,
         }}
+        name={{ name: name }}
       />
       {Platform.OS === "android" ? (
         <KeyboardAvoidingView behavior='height' />
